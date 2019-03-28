59767fa27dec838b9d115400c72ad7b6683cb347
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-28 20:10:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
 attr_reader :id
 attr_accessor :name, :grade
 
-  def initialize(name:, grade:, id: = nil)
+  def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
   end
